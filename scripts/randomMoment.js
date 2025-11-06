1eb5f7ad6ae31b5c4966adb6ca07fecc22026ed4
staitdarren <staitdarren@gmail.com>
2025-11-06 16:35:05 +0000

    A bunch of more blocks added

--- scripts/randomMoment.js
+++ scripts/randomMoment.js
@@ -7,11 +7,12 @@ if (randomMomentBtn) {
       // --- Aaron ---
       "aaron.html#modal-great-1", "aaron.html#modal-great-2", "aaron.html#modal-great-3",
       "aaron.html#modal-great-4", "aaron.html#modal-great-5", "aaron.html#modal-great-6",
-      "aaron.html#modal-great-7",
+      "aaron.html#modal-great-7", "aaron.html#modal-great-8",
       
       "aaron.html#modal-face-1", "aaron.html#modal-face-2", "aaron.html#modal-face-3",
       "aaron.html#modal-face-4", "aaron.html#modal-face-5", "aaron.html#modal-face-6",
-      "aaron.html#modal-face-7",
+      "aaron.html#modal-face-7", "aaron.html#modal-face-8", "aaron.html#modal-face-9",
+      "aaron.html#modal-face-10",
 
       "aaron.html#modal-funky-1", "aaron.html#modal-funky-2", "aaron.html#modal-funky-3",
       "aaron.html#modal-funky-4", "aaron.html#modal-funky-5", "aaron.html#modal-funky-6",
@@ -22,11 +23,14 @@ if (randomMomentBtn) {
       "aaron.html#modal-general-7", "aaron.html#modal-general-8", "aaron.html#modal-general-9",
       "aaron.html#modal-general-10", "aaron.html#modal-general-11", "aaron.html#modal-general-12",
       "aaron.html#modal-general-13", "aaron.html#modal-general-14", "aaron.html#modal-general-15",
-      "aaron.html#modal-general-16", "aaron.html#modal-generalg-17", "aaron.html#modal-general-18",
+      "aaron.html#modal-general-16", "aaron.html#modal-general-17", "aaron.html#modal-general-18",
       "aaron.html#modal-general-19", "aaron.html#modal-general-20", "aaron.html#modal-general-21",
-      "aaron.html#modal-general-22", "aaron.html#modal-generalg-23", "aaron.html#modal-general-24",
+      "aaron.html#modal-general-22", "aaron.html#modal-general-23", "aaron.html#modal-general-24",
       "aaron.html#modal-general-25", "aaron.html#modal-general-26", "aaron.html#modal-general-27",
-      "aaron.html#modal-general-28",
+      "aaron.html#modal-general-28", "aaron.html#modal-general-29", "aaron.html#modal-general-30",
+      "aaron.html#modal-general-31", "aaron.html#modal-general-32", "aaron.html#modal-general-33",
+      "aaron.html#modal-general-34", "aaron.html#modal-general-35", "aaron.html#modal-general-36",
+      "aaron.html#modal-general-37",
 
       "aaron.html#modal-wild-1", "aaron.html#modal-wild-2", "aaron.html#modal-wild-3",
       "aaron.html#modal-wild-4", "aaron.html#modal-wild-5", "aaron.html#modal-wild-6",
@@ -36,7 +40,8 @@ if (randomMomentBtn) {
       "aaron.html#modal-wild-16", "aaron.html#modal-wild-17", "aaron.html#modal-wild-18",
       "aaron.html#modal-wild-19", "aaron.html#modal-wild-20", "aaron.html#modal-wild-21", 
       "aaron.html#modal-wild-22", "aaron.html#modal-wild-23", "aaron.html#modal-wild-24",
-      "aaron.html#modal-wild-25",
+      "aaron.html#modal-wild-25", "aaron.html#modal-wild-26", "aaron.html#modal-wild-27",
+      "aaron.html#modal-wild-28", "aaron.html#modal-wild-29",
       
       // --- Heggie ---
       "heggie.html#modal-funky-1",
@@ -51,13 +56,13 @@ if (randomMomentBtn) {
       "heggie.html#modal-general-4", "heggie.html#modal-general-5", "heggie.html#modal-general-6",
       "heggie.html#modal-general-7", "heggie.html#modal-general-8", "heggie.html#modal-general-9",
       "heggie.html#modal-general-10", "heggie.html#modal-general-11", "heggie.html#modal-general-12",
-      "heggie.html#modal-general-13",
+      "heggie.html#modal-general-13", "heggie.html#modal-general-14", "heggie.html#modal-general-15",
       
       // --- Calum ---
       "calum.html#modal-funky-1", "calum.html#modal-funky-2", "calum.html#modal-funky-3",
       "calum.html#modal-funky-4", "calum.html#modal-funky-5",
 
-      "calum.html#modal-face-1",
+      "calum.html#modal-even-listening-1", "calum.html#modal-even-listening-2", "calum.html#modal-even-listening-3",
 
       "calum.html#modal-great-1", "calum.html#modal-great-2",
 
@@ -70,7 +75,8 @@ if (randomMomentBtn) {
       // --- Jacob ---
       "jacob.html#modal-general-1", "jacob.html#modal-general-2", "jacob.html#modal-general-3",
       "jacob.html#modal-general-4", "jacob.html#modal-general-5", "jacob.html#modal-general-6",
-      "jacob.html#modal-general-7",
+      "jacob.html#modal-general-7", "jacob.html#modal-general-8", "jacob.html#modal-general-9",
+      "jacob.html#modal-general-10", "jacob.html#modal-general-11", "jacob.html#modal-general-12",
       
       "jacob.html#modal-funky-1", "jacob.html#modal-funky-2", "jacob.html#modal-funky-3",
       "jacob.html#modal-funky-4", "jacob.html#modal-funky-5",
